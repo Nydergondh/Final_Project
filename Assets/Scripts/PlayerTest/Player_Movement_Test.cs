de966fa3b3bea78547adb00985ac8f16ada188af
Vinicius Rosa <viniciusr25@hotmail.com>
2020-12-14 04:24:26 -0300

    Replaced enemy rig

--- Assets/Scripts/PlayerTest/Player_Movement_Test.cs
+++ Assets/Scripts/PlayerTest/Player_Movement_Test.cs
@@ -14,8 +14,15 @@ public class Player_Movement_Test : MonoBehaviour
     private LayerMask groundMask;
 
     public float moveSpeed = 3f;
+    public float pushPower = 5f;
     [SerializeField]
     private float strafeTreshhold = 1.5f;
+
+    public float GroundDistance = 0.2f;
+    public LayerMask Ground;
+
+    private Vector3 _velocity;
+    public bool _isGrounded = false;
 
     // Start is called before the first frame update
     void Start() {
@@ -51,6 +58,12 @@ public class Player_Movement_Test : MonoBehaviour
             zSpeed = Input.GetAxis("Vertical") * -1;
         }
 
+
+        //Grounded Logic (to Do gravity)
+        _isGrounded = Physics.CheckSphere(transform.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+        if (_isGrounded && _velocity.y < 0)
+            _velocity.y = 0f;
+
         //do all the move logic and calculation if the player recive any movement input at all
         if (Input.GetButton("Horizontal") || Input.GetButton("Vertical")) {
             movement = new Vector3(xSpeed * moveSpeed, 0, zSpeed * moveSpeed);
@@ -58,31 +71,25 @@ public class Player_Movement_Test : MonoBehaviour
             if (Player_Test.player.timeSlowed) {
                 movement *= 2;//TODO IMPORTANT Change this to have more concistency (do math son)
             }
-            velocityVector = new Vector3(movement.x + transform.position.x, 0, movement.z + transform.position.z);
 
-            playerControler.SimpleMove(movement);
-
+            _velocity = new Vector3(movement.x , 0, movement.z);
             //TODO change latter to just have the above or this condicion
             playerAnim.SetMoving(true);
-
         }
         else {
-            velocityVector = Vector3.zero;
-            //playerAnim.SetMoving(false);
-
-            //TODO see the equivalent to this in the animator parameters
-            //if (Player_Test.player.isAttacking) {
-            //    //player.GetLegsAnim().SetAttack(false);
-            //}
-
+            _velocity = Vector3.zero;
             playerAnim.SetMoving(false);
-
         }
+        //add gravity
+        _velocity.y += Physics.gravity.y;
+
+        playerControler.Move(_velocity * Time.deltaTime);
+
         playerMesh.LookAt(pointToLook);
 
-        productVector = Vector3.Cross(pointToLook - transform.position, velocityVector - transform.position);
+        productVector = Vector3.Cross(pointToLook - transform.position, _velocity - transform.position);
         //if is moving and the y axis of the result cross product between velocity and pointToLook is bigger than a threshold then strafe 
-        if (Mathf.Abs(velocityVector.magnitude) > 0.1f && Mathf.Abs(productVector.y) > strafeTreshhold) {
+        if (Mathf.Abs(_velocity.magnitude) > 0.1f && Mathf.Abs(productVector.y) > strafeTreshhold) {
             //playerAnim.SetPlayerStrafe(true);
             //TODO change latter to just have the above or this condicion
             playerAnim.SetStrafe(true);
@@ -97,6 +104,32 @@ public class Player_Movement_Test : MonoBehaviour
         //Debug.DrawLine(transform.position, pointToLook, Color.red); // player to mouse
         //Debug.DrawLine(velocityVector, transform.position, Color.yellow); //player speed
         //Debug.DrawLine(velocityVector, pointToLook, Color.blue); // (mouse - player speed) vector
+    }
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic) {
+            return;
+        }
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3) {
+            return;
+        }
+
+        print("Called " + hit.collider.attachedRigidbody.name);
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.velocity = pushDir * pushPower;
     }
 
     //pass a vector to shrink (targetVector) to a magnitude
